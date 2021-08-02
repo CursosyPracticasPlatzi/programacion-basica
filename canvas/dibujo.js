e14caf9e7d405242f4564dca618384b375f9c801
jonacf <jonathancastilloflores@gmail.com>
2021-08-02 17:40:11 -0500

    avance dibujo canvas

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -10,6 +10,7 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 var lineas = 30;
+var linea = 15;
 var l = 0;
 var xf, yi, xi, yf;
 var retroxf, retroyf;
@@ -20,7 +21,7 @@ dibujarLinea("white", 1, 299, 299, 299);
 dibujarLinea("white", 1, 1, 299, 1);
 dibujarLinea("white", 299, 1, 299, 299);
 
-for (l = 0; l < lineas; l++) {
+while (l < lineas) {
   yi = 10 * l;
   xf = 10 * (l + 1);
   xi = l * 10;
@@ -30,5 +31,29 @@ for (l = 0; l < lineas; l++) {
   dibujarLinea(colorcito, xi, 0, 300, yf);
   dibujarLinea(colorcito, 300, yi, retroxf, 300);
   dibujarLinea(colorcito, retroxf, 0, 0, yf);
+
+  // dibujarLinea("green", 150, 150, xf, 300);
+  // dibujarLinea("green", 150,150, 300,yf);
+  // dibujarLinea("green", 150, 150,xi,0 );
+  // dibujarLinea("green", 150, 150, 0, yf);
   console.log("linea " + 1);
+  l++;
 }
+
+for (l=0; l < linea; l++) {
+  yi = 10 * l;
+  xf = 10 * (l + (32/2));
+  xi = l * 10;
+  yf = (l + (32/2)) * 10;
+  retroxf = 300 - (l*10);
+  retroyf = 150 - (l*10);
+  yff= 160+(l*10);
+  dibujarLinea("yellow", 150, yi, retroyf, 150); //superior-izquierda
+  dibujarLinea("red", 150, yi, xf, 150); //superior-derecha
+  dibujarLinea("blue", xi, 150, 150, yff); //inferior-izquierda
+  dibujarLinea("green", retroxf, 150, 150, yf); //inferior-derecha
+  console.log("linea" + 1)
+} 
+
+dibujarLinea("blue", 0,150,300,150)
+dibujarLinea("blue", 150,0,150, 300)
